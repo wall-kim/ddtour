1e713ca1418deefa7c7eb8864a350ef510889aa0
wall-kim <wall.kim@outlook.kr>
2018-11-07 21:35:53 +0900

    mobile view

--- ddtour/post/urls.py
+++ ddtour/post/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'mobile/', views.post_list_mobile, name='post_list_mobile'),
 ]
--- ddtour/post/views.py
+++ ddtour/post/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'post/post_list.html', {})
+
+def post_list_mobile(request):
+    return render(request, 'post/post_list_mobile.html', {})
