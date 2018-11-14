5ddf844d3801147d573751ecfba34a8ea0158c6c
wall-kim <wall.kim@outlook.kr>
2018-11-14 15:59:11 +0900

    form edit

--- ddtour/post/urls.py
+++ ddtour/post/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^mobile/$', views.post_list_mobile, name='post_list_mobile'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
 ]
