ae2d03f6687c2fd1a388d37a4934e5d289c21a83
wall-kim <wall.kim@outlook.kr>
2018-11-14 17:49:57 +0900

    publish and delete

--- ddtour/post/views.py
+++ ddtour/post/views.py
@@ -29,7 +29,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -46,7 +46,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -54,3 +54,17 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'post/post_edit.html', {'form' : form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'post/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
